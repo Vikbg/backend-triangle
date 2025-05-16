be33fd88d83a25b3b0bbc36187cec9989cc39e8a
Vikbg <viktorserhiienko12@gmail.com>
2025-05-16 19:36:40 +0200

    Tests passed

--- token-hook.js
+++
@@ -1,9 +0,0 @@
-// token-hook.js
-module.exports = {
-  beforeRequest: (req, context, ee, next) => {
-    if (req.headers && context.vars.authToken) {
-      req.headers.Authorization = `Bearer ${context.vars.authToken}`;
-    }
-    return next();
-  }
-};
