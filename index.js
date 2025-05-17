5cf3d61a1dc71c6f4229046cc6014dba90f954c1
Vikbg <viktorserhiienko12@gmail.com>
2025-05-17 10:16:18 +0200

    Minors Patches

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use('/players', playerRoutes);
 app.use('/scores', scoreRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Serveur démarré sur http://0.0.0.0:${PORT}`);
 });
 
